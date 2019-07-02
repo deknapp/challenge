105380223970ece7cb1cb256b77a6ac5097e0818
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2019-07-01 18:26:09 -0800

    rough draft

--- code.py
+++ code.py
@@ -3,33 +3,38 @@ import os
 import struct
 import sys
 
-# gets a string type from the byte value
-def get_type(byte):
-  if byte == 0x00:
-    return 'Debit'
-  elif byte == 0x01:
-    return 'Credit'
-  elif byte == 0x02:
-    return 'StartAutopay'
-  elif byte == 0x03:
-    return 'EndAutopay'
-  else:
-    print('ERROR: invalid byte for type ' + byte) 
+MAGIC_USER_ID = 2456938384156277127
 
-# Returns dictionary of records
 def read_file(file_name):
   handle = open(file_name, 'rb')
   file_content = handle.read() 
   version = file_content[4]
   num_records = int.from_bytes(file_content[5:9]) 
-  records = {}
+  
   debit_total = 0
   credit_total = 0
   num_autopays_started = 0
   num_autopays_ended = 0 
+  
   for i in range(10:13:len(file_content)):
-    record_type = get_type(file_content[i])
-    
+    record_type = file_content[i]
+    timestamp = int.from_bytes(file_content[i+1:i+5])
+    user_id = int.from_bytes(file_content[i+5:i+13])    
+    if record_type == 0x01:
+      dollar_amount = float.from_bytes(file_content[i+13:i+21])
+      credit_total += dollar_amount
+    elif record_type == 0x00:
+      dollar_amount = float.from_bytes(file_content[i+13:i+21])
+      debit_total += dollar_amount
+    elif record_type == 0x02:
+      num_autopays_started += 1
+    elif record_type == 0x03:
+      num_autopays_ended += 1
+    else:
+      print('ERROR: invalid record type ' + record_type)
+    if user_id == MAGIC_USER_ID:
+      if record_type == 0x01 or record_type == 0x00:
+        user_balance += int.from_bytes(file_content[i+13:i+21])
   
   print 'Total amount in dollars of debits: ' + debit_total 
   print 'Total amount in dollars of credits: ' + credit_total
