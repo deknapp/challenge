391bb711dbd4519f2b88a3d5f97cf37eea5d6d5f
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2019-07-02 15:26:10 -0800

    more cleanup

--- code.py
+++ code.py
@@ -5,6 +5,13 @@ import sys
 
 MAGIC_USER_ID = 2456938384156277127
 
+DEBIT_TYPE = 0x01
+CREDIT_TYPE = 0x00
+AUTOPAY_START_TYPE = 0x02 
+AUTOPAY_END_TYPE = 0x03 
+AUTOPAY_RECORD_LENGTH = 13
+TRANSACTION_RECORD_LENGTH = 21
+
 def read_file(file_name):
   handle = open(file_name, 'rb')
   file_content = handle.read() 
@@ -15,29 +22,31 @@ def read_file(file_name):
   credit_total = 0
   num_autopays_started = 0
   num_autopays_ended = 0 
-  i = 0
-  
+  i = 4
   for record in range(num_records): 
     record_type = file_content[i]
     timestamp = int.from_bytes(file_content[i+1:i+5], byteorder='big')
     user_id = int.from_bytes(file_content[i+5:i+13], byteorder='big')    
-    if user_id == MAGIC_USER_ID:
-      if record_type == 0x01 or record_type == 0x00:
-        user_balance += int.from_bytes(file_content[i+13:i+21], byteorder='big')
-    if record_type == 0x01:
-      dollar_amount = float.from_bytes(file_content[i+13:i+21], byteorder='big')
+    dollar_amount = 0 
+    print('record type: ' + str(record_type))
+    print('timestamp: ' + str(timestamp))
+    print('user_id:'  + str(user_id)) 
+    if record_type in [CREDIT_TYPE, DEBIT_TYPE]:
+      dollar_amount = int.from_bytes(file_content[i+13:i+21], byteorder='big') 
+      if user_id == MAGIC_USER_ID:
+        user_balance += dollar_amount 
+    if record_type == CREDIT_TYPE:
       credit_total += dollar_amount
-      i += 21
-    elif record_type == 0x00:
-      dollar_amount = float.from_bytes(file_content[i+13:i+21], byteorder='big')
+      i += TRANSACTION_RECORD_LENGTH
+    elif record_type == DEBIT_TYPE:
       debit_total += dollar_amount
-      i += 21
-    elif record_type == 0x02:
+      i += TRANSACTION_RECORD_LENGTH
+    elif record_type == AUTOPAY_START_TYPE:
       num_autopays_started += 1
-      i += 13
-    elif record_type == 0x03:
+      i += AUTOPAY_RECORD_LENGTH 
+    elif record_type == AUTOPAY_END_TYPE:
       num_autopays_ended += 1
-      i += 13
+      i += AUTOPAY_RECORD_LENGTH
     else:
       print('ERROR: invalid record type ' + str(record_type))
       exit() 
